1bf6400efbc00782d338229ae3061f0cb85011c5
Leonardo Barbieri Bedendo <leonardobdn@gmail.com>
2021-04-03 19:16:30 -0300

    Ignora alguns campos no mapeamento de DTO para dominio

--- app/src/main/java/io/platosedu/adapter/out/persistence/mapper/ContentMapper.java
+++ app/src/main/java/io/platosedu/adapter/out/persistence/mapper/ContentMapper.java
@@ -5,6 +5,7 @@ import io.platosedu.adapter.in.dto.response.ContentResponse;
 import io.platosedu.domain.Content;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(
         componentModel = "jsr330",
@@ -12,8 +13,17 @@ import org.mapstruct.Mapper;
                 DocumentMapper.class},
         injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface ContentMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "deletedAt", ignore = true)
     Content fromContentRequest(ContentRequest contentRequest, Boolean active, String tenantId, String createdByUserId);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "deletedAt", ignore = true)
+    @Mapping(target = "createdByUserId", ignore = true)
     Content fromContentRequest(ContentRequest contentRequest, Boolean active, String tenantId);
 
     ContentResponse toContentResponse(Content content);
